a8e83ff32af5846216892adaabcb41834545949d
Ai Uragaki <ai.uragaki.nz@gmail.com>
2021-06-15 18:00:11 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NestedLoopPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+
+
+                    Console.Write(j);
+                   
+                }
+                Console.WriteLine(""); //make space between loop
+
+
+            }
+
+        }
+    }
+}
